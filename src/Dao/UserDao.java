7887839ffbcc79ff22789d43cc44126876da2a0e
erin <erin314@naver.com>
2015-05-03 15:13:57 +0900

    #  connection분리

--- src/Dao/UserDao.java
+++ src/Dao/UserDao.java
@@ -1,19 +1,25 @@
 package Dao;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import Model.User;
 
 public abstract class UserDao {
+	private SimpleConnectionMaker simpleConnectionMaker;
 	
-	public void create(User user ){
-		Connection c = getConnection();
+	public UserDao() {
+		simpleConnectionMaker = new SimpleConnectionMaker();
+	}
+	
+	public void create(User user ) throws ClassNotFoundException, SQLException{
+		Connection c = simpleConnectionMaker.makeNewConnection();
 //		insert
 	}
-	public void recieve(User user){
-		Connection c = getConnection();
-	}
 	
-	protected abstract Connection getConnection() ;
+	public void recieve(User user) throws ClassNotFoundException, SQLException{
+		Connection c = simpleConnectionMaker.makeNewConnection();
+//		recieve
+	}
 	
 }
