d19beb33ba52b5da53b6c1a0b3a852f8f3765f7e
erin <erin314@naver.com>
2015-05-03 17:18:11 +0900

    CountingConnectMaker to Spring

--- src/Dao/CountingDaoFactory.java
+++ src/Dao/CountingDaoFactory.java
@@ -4,11 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class DaoFactory {
+public class CountingDaoFactory {
 	
 	@Bean
 	public UserDao userDao() {
-		return new UserDao(realConnectionMaker());
+		return new UserDao(connectionMaker());
+	}
+	@Bean
+	public ConnectionMaker connectionMaker(){
+		return new CountingConnectionMaker(realConnectionMaker());
 	}
 	
 	@Bean
--- test/UserDaoTest.java
+++ test/UserDaoTest.java
@@ -1,10 +1,15 @@
+import static org.junit.Assert.*;
+
 import java.sql.SQLException;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import Dao.CountingConnectionMaker;
+import Dao.CountingDaoFactory;
 import Dao.DaoFactory;
+import Dao.ProductDBConnectionMaker;
 import Dao.UserDao;
 import Model.User;
 
@@ -20,6 +25,19 @@ public class UserDaoTest {
 		
 		userDao.create(user);
 		userDao.recieve(user);
+	}
+	@Test
+	public void countingConnectionMaker() throws Exception {
+		ApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+		UserDao userDao = context.getBean("userDao", UserDao.class);
+		CountingConnectionMaker c = context.getBean("connectionMaker", CountingConnectionMaker.class);
+		
+		User user = new User();
+		System.out.println(c.getCounter());
+		userDao.create(user);
+		System.out.println(c.getCounter());
+		userDao.recieve(user);
+		System.out.println(c.getCounter());
 	}
 
 
