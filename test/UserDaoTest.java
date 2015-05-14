4b01179dc526bffd849e4f047bdf45dfb79233c7
erin <erin314@naver.com>
2015-05-14 21:16:26 +0900

    spring & junit : contextConfiguration

--- test/UserDaoTest.java
+++ test/UserDaoTest.java
@@ -4,21 +4,26 @@ import java.sql.SQLException;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import Dao.UserDao;
 import Model.User;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/applicationContext.xml")
 public class UserDaoTest {
 
+	@Autowired
 	private ApplicationContext context;
 	private UserDao userDao;
 	
 	@Before
 	public void setUp(){
-		context = new GenericXmlApplicationContext("/applicationContext.xml");
 		this.userDao = context.getBean("userDao", UserDao.class);
 	}
 	
