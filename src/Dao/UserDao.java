95c231531a2607f9957dd1d40a56665c7980922a
erin <erin314@naver.com>
2015-05-17 14:46:26 +0900

    [3장] 예외 처리 해주기 try catch

--- src/Dao/UserDao.java
+++ src/Dao/UserDao.java
@@ -53,12 +53,33 @@ public class UserDao {
 	}
 
 	public void deleteAll() throws SQLException {
-		Connection c = dataSource.getConnection();
-		PreparedStatement ps = c.prepareStatement("delete from users");
-		ps.executeUpdate();
+		Connection c = null;
+		PreparedStatement ps = null;
+		try {
+			c = dataSource.getConnection();
+			ps = c.prepareStatement("delete from users");
+			ps.executeUpdate();
 
-		ps.close();
-		c.close();
+			ps.close();
+			c.close();
+			
+		} catch (SQLException e) {
+			throw e;
+		} finally{
+			if(ps!= null){
+				try {
+					ps.close();
+				} catch (SQLException e) {
+				}
+			}
+			if (c != null) {
+				try {
+					c.close();
+				} catch (SQLException e) {
+				}
+			}
+		}
+		
 	}
 
 	public int getCount() throws SQLException {
